53bea892bf35387b6b44bae1b12f2660ebb219a8
Petyr Nyagolov <petyrnyagolov@gmail.com>
2018-05-11 15:51:50 +0300

    add products, categories and etc

--- app/src/main/java/com/example/pepoco/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/pepoco/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pepoco.coderswag
+package com.example.pepoco.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pepoco.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/example/pepoco/coderswag/Model/Category.kt
@@ -0,0 +1,3 @@
+package com.example.pepoco.coderswag.Model
+
+class Category (val title: String, val image: String)
